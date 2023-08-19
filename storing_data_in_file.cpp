e5fea47ea7fbbd059ccba87c2d60bd5c02a53bb4
mehedi hasan ratul <128135301+mehedipro3@users.noreply.github.com>
2023-08-20 01:40:53 +0600

    Create storing_data_in_file.cpp

---
+++ storing_data_in_file.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<string>
+#include<fstream>
+using namespace std;
+int main()
+{
+    string name;
+    int age;
+
+    ofstream file;
+    file.open("student_details.txt",ios::out|ios::app);
+    for (int i=0; i<3; i++)
+    {
+
+        cout << "Enter your name :" ;
+        getline(cin,name);
+
+        file <<"Name :" << name << "\n";
+        cout  << "enter your age : ";
+        cin>>age;
+
+        file <<"Age:"<< age << endl;
+        cin.ignore();
+    }
+
+    file.close();
+    return 0;
+}
